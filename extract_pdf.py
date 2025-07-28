bb18d7c1b19eef75e9971273f0f11c0669eb4fd5
Ashis Kumar Ray <ashisray.057@gmail.com>
2025-07-28 13:12:11 +0530

    extract_pdf.py

--- extract_pdf.py
+++ extract_pdf.py
@@ -1,15 +1,6 @@
-# import PyPDF2
-
-# # Open the PDF file in binary read mode
-# with open(r"C:\Users\ashis\OneDrive\Desktop\Data Automation\pdf_automation\sample.pdf", "rb") as file:
-#     reader = PyPDF2.PdfReader(file)
-#     page = reader.pages[0]
-#     text = page.extract_text()
-#     print(text)
-
 import PyPDF2
-with open(r"C:\Users\ashis\OneDrive\Desktop\Data Automation\pdf_automation\sample.pdf","rb") as file:
+with open(r"C:\Users\ashis\OneDrive\Desktop\Data Automation\pdf_automation\sample.pdf","rb") as file:  
     reader = PyPDF2.PdfReader(file)
-    page =reader.pages[0]
+    page =reader.pages[0]        # This is will open the 1st page only
     text = page.extract_text()
     print (text)
